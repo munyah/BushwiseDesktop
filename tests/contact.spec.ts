6be85e5723a3ea12cd683d0db161f63cbedfe5ff
Tinago Mahachi <tinago.mahachi@gviprograms.com>
2024-01-11 08:19:58 +0200

    Added a test to verify menu links

--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -1,39 +1,51 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Contact', () => {
-    test('Fill contact form and verify success message', async ({ page }) => {
-        // open contact page
-        await page.goto('https://www.bushwise.co.za/contact-us');
+test.describe("Contact", () => {
+  test("Fill contact form and verify success message", async ({ page }) => {
+    // open contact page
+    await page.goto("https://www.bushwise.co.za/contact-us");
 
-        await page.waitForTimeout(3000);
+    await page.waitForTimeout(4000);
 
-        // Using name
-        const frame1 = await page.frame('htmlComp-iframe')
+    // Using name
+    const frame1 = page.frame("htmlComp-iframe");
 
-        // fill out the input fields
-        await frame1?.fill('input[placeholder="First Name*"]', 'Automated');
-        await frame1?.fill('input[placeholder="Last Name*"]', 'Test');
-        await frame1?.fill('input[placeholder="Email*"]', 'automatedtest@gviprograms.com');
-        await frame1?.fill('input[placeholder="Phone*"]', '07123456789');
-        await frame1?.selectOption('select[name="country"]', { value: 'United Kingdom' });
-        await frame1?.fill('textarea', 'This is just a test');
-        
-        // select checkboxes
-        await frame1?.click('input#tc_agree');
-        await frame1?.click('input#newsletter_signup');
+    // Wait for the frame to be visible with a timeout of 10 seconds
+    // await page.waitForSelector('iframe[name="htmlComp-iframe"]', { state: 'visible', timeout: 30000 });
 
-       // click submit
-        const submitButton = await frame1?.$("button[type='submit']");
+    // fill out the input fields
+    await frame1?.fill('input[placeholder="First Name*"]', "Automated");
+    await frame1?.fill('input[placeholder="Last Name*"]', "Test");
+    await frame1?.fill(
+      'input[placeholder="Email*"]',
+      "automatedtest@gviprograms.com"
+    );
+    await frame1?.fill('input[placeholder="Phone*"]', "07123456789");
+    await frame1?.selectOption('select[name="country"]', {
+      value: "United Kingdom",
+    });
+    await frame1?.fill("textarea", "This is just a test");
 
-        // click submit again (retry 2 times)
-        for (let i = 0; i < 2; i++) {
-        await page.waitForTimeout(2000);
-        await submitButton?.click();
-        }
-        
-        // verify success message
-        await frame1?.waitForSelector("//h1[normalize-space()='Thank you for reaching out to Bushwise.']", { state: 'visible' })
-        const successAlert = await frame1?.$("//h1[normalize-space()='Thank you for reaching out to Bushwise.']")
-        expect(successAlert).toBeTruthy
-    })
-})
+    // select checkboxes
+    await frame1?.click("input#tc_agree");
+    await frame1?.click("input#newsletter_signup");
+
+    // click submit
+    const submitButton = await frame1?.$("button[type='submit']");
+
+    // click submit again (retry 2 times)
+    for (let i = 0; i < 2; i++) {
+      await submitButton?.click();
+    }
+
+    // verify success message
+    const successAlert = await frame1?.waitForSelector(
+      '//h1[normalize-space()="Thank you for reaching out to Bushwise."]',
+      { state: "visible" }
+    );
+    expect(successAlert).toBeTruthy();
+    expect(await successAlert?.textContent()).toContain(
+      "Thank you for reaching out to Bushwise."
+    );
+  });
+});
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -24,7 +24,7 @@ test.describe('Home', () => {
         // click the button 
         await page.locator("a[aria-label='Go into the wild'] div[class='StylableButton2545352419__container']").click();
 
-        // verify url has #get-started
+        // verify url has #wildlife-courses
         // await expect(page).toHaveURL('https://www.bushwise.co.za/wildlife-courses');
         await expect(page).toHaveURL(/.*wildlife-courses/);
         
@@ -40,6 +40,45 @@ test.describe('Home', () => {
 
         // verify heading text is visible
         await expect(headingText).toBeVisible();
+        
+    })
+
+    test('Verify Bushwise name and logo is visible using xpath selector', async ({ page }) => {
+        // open url
+        await page.goto('https://www.bushwise.co.za/');
+
+        // find the name and logo 
+        const nameAndLogo = page.locator('//*[@id="comp-ln0yip0a_r_comp-lm8z9wee"]');
+
+        // verify name and logo is visible
+        await expect(nameAndLogo).toBeVisible();
+        
+    })
+
+    test('Verify text of all nav links', async ({ page }) => {
+        const expectedLinks = [
+            "ONLINE COURSES",
+    "FGASA Field Guiding online course",
+    "Nature Enthusiast online course",
+    "Introduction Field Guiding online course",
+        ];
+
+        // open url
+        await page.goto('https://www.bushwise.co.za/');
+
+        // find the nav links
+        // const navLinks = page.locator('//*[@class="StylableHorizontalMenu3372578893__menu"]//*[@class="itemDepth02233374943__itemWrapper wixui-horizontal-menu__item wixui-horizontal-menu__submenu-title"]');
+        const navLinks = page.locator('//*[@class="StylableHorizontalMenu3372578893__menu"]//*[@class="itemDepth02233374943__itemWrapper wixui-horizontal-menu__item wixui-horizontal-menu__submenu-title"]').nth(1);
+        // const navLinks = page.locator('#zak-primary-menu li[id*=menu]');
+
+        // print out all the links
+        for(const el of await navLinks.elementHandles()){
+            console.log(await el.textContent());
+        }
+
+        // verify nav links text
+        // expect(await navLinks.allTextContents()).toEqual(expectedLinks);
+        expect(await navLinks.textContent()).toContain(expectedLinks[1]);
         
     })
 })
