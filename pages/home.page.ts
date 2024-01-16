96ec87ef405ba932551837d1e719c15e5a86c954
Tinago Mahachi <tinago.mahachi@gviprograms.com>
2024-01-16 20:55:33 +0200

    Added HomePage locators

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,7 +1,16 @@
 class HomePage {
     page: any;
+    goIntoTheWildBtn: any;
+    headingText: any;
+    nameAndLogo: any;
+    navLinks: any;
+    
     constructor(page){
         this.page = page;
+        this.goIntoTheWildBtn = page.locator("a[aria-label='Go into the wild'] div[class='StylableButton2545352419__container']");
+        this.headingText = page.locator('text=Choose your');
+        this.nameAndLogo = page.locator('//*[@id="comp-ln0yip0a_r_comp-lm8z9wee"]');
+        this.navLinks = page.locator('//*[@class="StylableHorizontalMenu3372578893__menu"]//*[@class="itemDepth02233374943__itemWrapper wixui-horizontal-menu__item wixui-horizontal-menu__submenu-title"]');
 
     }
 
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -5,7 +5,7 @@ test.describe('Home', () => {
     let homePage;
     test('Open HomePage and verify title', async ({ page }) => {
         homePage = new HomePage(page);
-        
+
         // open url
         await page.goto('https://www.bushwise.co.za/');
 
@@ -14,6 +14,8 @@ test.describe('Home', () => {
     })
 
     test('Open About us page and verify title', async ({ page }) => {
+        homePage = new HomePage(page);
+
         // open url
         await page.goto('https://www.bushwise.co.za/about-us');
 
@@ -22,11 +24,13 @@ test.describe('Home', () => {
     })
 
     test('Click the Go into the wild button using CSS Selector', async ({ page }) => {
+        homePage = new HomePage(page);
+
         // open url
         await page.goto('https://www.bushwise.co.za/');
 
         // click the button 
-        await page.locator("a[aria-label='Go into the wild'] div[class='StylableButton2545352419__container']").click();
+        await homePage.goIntoTheWildBtn.click();
 
         // verify url has #wildlife-courses
         // await expect(page).toHaveURL('https://www.bushwise.co.za/wildlife-courses');
@@ -36,11 +40,13 @@ test.describe('Home', () => {
 
 
     test('Verify heading text is visible using text selector', async ({ page }) => {
+        homePage = new HomePage(page);
+
         // open url
         await page.goto('https://www.bushwise.co.za/');
 
         // find the text locator  
-        const headingText = page.locator('text=Choose your');
+        const headingText = homePage.headingText;
 
         // verify heading text is visible
         await expect(headingText).toBeVisible();
@@ -48,11 +54,13 @@ test.describe('Home', () => {
     })
 
     test('Verify Bushwise name and logo is visible using xpath selector', async ({ page }) => {
+        homePage = new HomePage(page);
+
         // open url
         await page.goto('https://www.bushwise.co.za/');
 
         // find the name and logo 
-        const nameAndLogo = page.locator('//*[@id="comp-ln0yip0a_r_comp-lm8z9wee"]');
+        const nameAndLogo = homePage.nameAndLogo;
 
         // verify name and logo is visible
         await expect(nameAndLogo).toBeVisible();
@@ -60,6 +68,7 @@ test.describe('Home', () => {
     })
 
     test('Verify text of all nav links', async ({ page }) => {
+        homePage = new HomePage(page);
         const expectedLinks = [
             "ONLINE COURSES",
     "FGASA Field Guiding online course",
@@ -72,8 +81,9 @@ test.describe('Home', () => {
 
         // find the nav links
         // const navLinks = page.locator('//*[@class="StylableHorizontalMenu3372578893__menu"]//*[@class="itemDepth02233374943__itemWrapper wixui-horizontal-menu__item wixui-horizontal-menu__submenu-title"]');
-        const navLinks = page.locator('//*[@class="StylableHorizontalMenu3372578893__menu"]//*[@class="itemDepth02233374943__itemWrapper wixui-horizontal-menu__item wixui-horizontal-menu__submenu-title"]').nth(1);
+        // const navLinks = page.locator('//*[@class="StylableHorizontalMenu3372578893__menu"]//*[@class="itemDepth02233374943__itemWrapper wixui-horizontal-menu__item wixui-horizontal-menu__submenu-title"]').nth(1);
         // const navLinks = page.locator('#zak-primary-menu li[id*=menu]');
+        const navLinks = homePage.navLinks.nth(1);
 
         // print out all the links
         for(const el of await navLinks.elementHandles()){
