e752f8e5629c4f1599336f7ac63715bc70cb8bc9
Tinago Mahachi <tinago.mahachi@gviprograms.com>
2024-01-18 12:37:14 +0200

    Created page objects for the contact page

---
+++ pages/contact.page.ts
@@ -0,0 +1,57 @@
+import { Page, FrameLocator, Locator } from "@playwright/test";
+
+class ContactPage {
+  private page: Page;
+  frame1: FrameLocator;
+  successTextLocator: Locator;
+
+  // Define static constants for locators
+  static iframeLoactor = "htmlComp-iframe";
+  static thankYouMessage =
+    '//h1[normalize-space()="Thank you for reaching out to Bushwise."]';
+
+  constructor(page: Page) {
+    this.page = page;
+    this.frame1 = page.frameLocator("htmlComp-iframe");
+    this.frame1 = page.frameLocator(ContactPage.iframeLoactor);
+    // Set successText locator within the frame
+    this.successTextLocator = this.frame1.locator(ContactPage.thankYouMessage);
+    // Set successText locator within the frame
+    // this.successTextLocator = this.frame1.locator('//h1[normalize-space()="Thank you for reaching out to Bushwise."]');
+  }
+
+  async navigate() {
+    await this.page.goto("https://www.bushwise.co.za/contact-us");
+  }
+
+  async submitForm(
+    firstName: string,
+    lastName: string,
+    email: string,
+    phone: string,
+    country: string,
+    message: string
+  ) {
+    const frame1 = await this.page.frame("htmlComp-iframe");
+
+    // fill out the input fields
+    await frame1?.fill('input[placeholder="First Name*"]', firstName);
+    await frame1?.fill('input[placeholder="Last Name*"]', lastName);
+    await frame1?.fill('input[placeholder="Email*"]', email);
+    await frame1?.fill('input[placeholder="Phone*"]', phone);
+    await frame1?.selectOption('select[name="country"]', { value: country });
+    await frame1?.fill("textarea", message);
+
+    // select checkboxes
+    await frame1?.click("input#tc_agree");
+    await frame1?.click("input#newsletter_signup");
+
+    // click submit (retry 2 times)
+    const submitButton = await frame1?.$("button[type='submit']");
+    for (let i = 0; i < 2; i++) {
+      await submitButton?.click();
+    }
+  }
+}
+
+export default ContactPage;
--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -1,50 +1,34 @@
+// contact.spec.ts
+
 import { test, expect } from "@playwright/test";
+import ContactPage from "../pages/contact.page";
 
-test.describe("Contact", () => {
-  test("Fill contact form and verify success message", async ({ page }) => {
-    // open contact page
-    await page.goto("https://www.bushwise.co.za/contact-us");
+test("Fill contact form and verify success message", async ({ page }) => {
+  const contactPage = new ContactPage(page);
 
-    // Access the Playwright Inspector via code
-    await page.pause();
+  // navigate to contact page
+  await contactPage.navigate();
+  await page.waitForTimeout(4000);
 
-    await page.waitForTimeout(4000);
+  // fill out the input fields and submit
+  await contactPage.submitForm(
+    "Automated",
+    "Test",
+    "automatedtest@gviprograms.com",
+    "07123456789",
+    "United Kingdom",
+    "This is just a test"
+  );
 
-    // Using name
-    const frame1 = await page.frame("htmlComp-iframe");
+  // Using the static constants
+  const frame1 = page.frame(ContactPage.iframeLoactor);
+  const successAlert = await frame1?.waitForSelector(
+    ContactPage.thankYouMessage,
+    { state: "visible" }
+  );
 
-
-    // fill out the input fields
-    await frame1?.fill('input[placeholder="First Name*"]', "Automated");
-    await frame1?.fill('input[placeholder="Last Name*"]', "Test");
-    await frame1?.fill(
-      'input[placeholder="Email*"]',
-      "automatedtest@gviprograms.com"
-    );
-    await frame1?.fill('input[placeholder="Phone*"]', "07123456789");
-    await frame1?.selectOption('select[name="country"]', {
-      value: "United Kingdom",
-    });
-    await frame1?.fill("textarea", "This is just a test");
-
-    // select checkboxes
-    await frame1?.click("input#tc_agree");
-    await frame1?.click("input#newsletter_signup");
-
-    // click submit (retry 2 times)
-    const submitButton = await frame1?.$("button[type='submit']");
-    for (let i = 0; i < 2; i++) {
-      await submitButton?.click();
-    }
-
-    // verify success message
-    const successAlert = await frame1?.waitForSelector(
-      '//h1[normalize-space()="Thank you for reaching out to Bushwise."]',
-      { state: "visible" }
-    );
-    expect(successAlert).toBeTruthy();
-    expect(await successAlert?.textContent()).toContain(
-      "Thank you for reaching out to Bushwise."
-    );
-  });
+  expect(successAlert).toBeTruthy();
+  expect(await successAlert?.textContent()).toContain(
+    "Thank you for reaching out to Bushwise."
+  );
 });
